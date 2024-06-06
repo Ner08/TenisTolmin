ba0056ca1d115ab3f5b86187fb0e440be4478974
Nejc Robič <nejc.robic@pronet-kr.si>
2024-06-06 15:45:59 +0200

    Narejen contacts form

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -5,11 +5,11 @@
     {{-- Informacije za stik --}}
     <section class="py-6">
         <div class="container mx-auto">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mx-2">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mx-4">
                 <div>
-                    <h2 class="text-2xl text-center font-bold bg-gray-500 p-2 rounded-t-xl text-gray-100">Informacije
+                    <h2 class="text-2xl text-center font-bold bg-sky-700 p-2 rounded-t-xl text-gray-100">Informacije
                     </h2>
-                    <div class="bg-gray-100  p-6 pb-1 rounded-t-lg shadow-md md:order-2">
+                    <div class="bg-gray-100  p-6 pb-1 shadow-md md:order-2">
 
                         <p class="text-gray-700"> <b>Naslov:</b> Dijaška ulica 12c</p>
                         <p class="text-gray-700"><b>Mesto:</b> Tolmin, Slovenija, 5220</p>
@@ -28,9 +28,33 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div>
+                <div>
+                    <h2 class="text-2xl text-center font-bold bg-gray-700 p-2 rounded-t-xl text-gray-100">Kontaktirajte nas
+                    </h2>
+                    <form action="{{ route('send_email') }}" method="POST" class="bg-gray-200 p-6 rounded-b-xl shadow-md mb-6">
+                        @csrf
+                        <div class="mb-4">
+                            <label for="email" class="block text-sm font-medium text-gray-700">Elektronski naslov*</label>
+                            <input type="email" id="email" name="email"
+                                class="mt-1 p-2 block w-full border-gray-300 rounded-md shadow-sm focus:border-gray-500 focus:ring focus:ring-gray-200 focus:ring-opacity-50"
+                                required>
+                        </div>
+                        <div class="mb-4">
+                            <label for="content" class="block text-sm font-medium text-gray-700">Sporočilo*</label>
+                            <textarea name="content" id="content"
+                            class="form-textarea mt-1 p-2 rounded-lg w-full h-28 focus:outline-none  border-gray-300 py-3 px-4" required></textarea>
+                        </div>
 
+                        <div class="mb-5">
+                            <p> <b class="text-gray-700">Pošljite nam sporočilo </b> in odgovorili vam bomo v najkrajšem možnem času.</p>
+                        </div>
+
+                        <div class="">
+                            <button type="submit"
+                                class="w-full px-4 py-2 bg-gray-700 text-white font-semibold rounded-md shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2">Pošlji</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </section>
@@ -38,7 +62,7 @@
     {{-- Dodatne Informacije za Stik --}}
     <section class="bg-gray-200 py-6">
         <div class="container mx-auto ">
-            <h2 class="text-3xl mx-2 font-bold mb-4">Kontaktirajte nas</h2>
+            <h2 class="text-3xl mx-2 font-bold mb-4">Drugi načini kontakta</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mx-2">
                 <div class="bg-white p-6 rounded-lg shadow-md">
                     <h3 class="text-xl font-semibold mb-2">Email</h3>
